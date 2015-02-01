882dab933dad1259fb500719e9553302f2ac6ee9
Benoit Petit <benoit.petit.info@gmail.com>
2015-02-01 22:30:10 +0100

    Add -- Generator now write into files
    instead of printing the result of rendering.

--- generator/generator.py
+++ generator/generator.py
@@ -3,26 +3,43 @@
 from jinja2 import Environment, FileSystemLoader
 from  generator.loader.json_loader import JsonLoader
 from pprint import pprint
+from os.path import isfile
 
 class Generator(object):
 
     def __init__(self, content_path, templates_path, output_path):
         self.__content = JsonLoader(content_path).get_content()
         self.__templates = TemplateLoader(templates_path)
+        if not output_path[-1] == '/':
+            output_path += '/'
+        self.__output_path = output_path
 
     def generate(self):
-        print(
+        self.__gen_devices_list()
+        self.__gen_device()
+
+    def __open_target_file(self, name):
+        full_path = self.__output_path + name
+        return open(full_path, "w")
+
+    def __gen_devices_list(self):
+        fd = self.__open_target_file("devices.html")
+        fd.write(
             self.__templates.get_template('devices.html').render(
                devices=self.__content['data']['device'].keys()
             )
         )
+        fd.close()
+
+    def __gen_device(self):
         for name, data in self.__content['data']['device'].items():
-            print(
-                name + ': ' +
+            fd = self.__open_target_file(name + ".html")
+            fd.write(
                 self.__templates.get_template('device.html').render(
                     data=data
                 )
             )
+            fd.close()
 
 class TemplateLoader(object):
 
